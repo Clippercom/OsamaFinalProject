655947f320382cce53f3e12d9e8df7d18ed833b7
Owner <ekrmaawad231@gmail.com>
2022-02-15 12:24:27 +0200

    ...

--- app/src/main/java/MyData/MyTaskAdapter.java
+++ app/src/main/java/MyData/MyTaskAdapter.java
@@ -1,6 +1,7 @@
 package MyData;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -20,7 +21,12 @@ public class MyTaskAdapter extends ArrayAdapter
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View v=View.inflate( getContext(), R.layout.task_item_layout,parent );
+        //View v=View.inflate( getContext(), R.layout.task_item_layout,parent );
+        //بناء واجهة لمعطى واحد
+        View v= LayoutInflater.from(getContext()).inflate(R.layout.task_item_layout,parent,false);
+
         return v;
+        MyTask item= getItem( position );
+
     }
 }
